81c71852765246592ea45b9762985a1ea3d004ff
mohitpanwar7 <mohit5637630@gmail.com>
2021-03-24 19:50:29 +0530

    state and country_id ip created

---
+++ front/src/pages/userbystate/userbystate.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {} from "react-bootstrap";
+
+class State extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state= {
+            states = []
+        }
+    }
+
+    
+
+}
